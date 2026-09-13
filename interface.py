74f59eea700656cf618b3494d2787a9e5bb6e6b0
VeggieVampire <farrow.nick@gmail.com>
2026-09-13 11:26:31 -0500

    Preserve listener logs across restarts

--- interface.py
+++ interface.py
@@ -32,7 +32,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler(LOG_FILE, mode="w"),
+        logging.FileHandler(LOG_FILE, mode="a"),
         logging.StreamHandler()
     ]
 )
